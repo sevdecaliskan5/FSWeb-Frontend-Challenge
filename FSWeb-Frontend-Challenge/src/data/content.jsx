55bd5e809182f813daff30132d28476d5f80d0ff
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 14:44:58 +0300

    profile component icerigi contente eklendi

--- FSWeb-Frontend-Challenge/src/data/content.jsx
+++ FSWeb-Frontend-Challenge/src/data/content.jsx
@@ -10,7 +10,18 @@ export const content = {
         github: "GitHub",
       },
       toggleLanguage: "🇹🇷 TR",
-      skillsTitle: "Skills"
+      skillsTitle: "Skills",
+      profile: {
+        basicInfo: "Basic Information",
+        birthDate: "Birth Date",
+        city: "City",
+        education: "Education",
+        educationDetails: "Pamukkale University, Civil Engineering, 2019",
+        preferredRole: "Preferred Role",
+        aboutMe: "About Me",
+        aboutDescription:
+          "I am a front-end developer with a strong passion for crafting scalable and user-friendly web applications. I enjoy working with modern UI frameworks and love transforming designs into fully functional products.",
+      },
     },
     tr: {
       greeting: "Merhaba! 👋",
@@ -23,7 +34,18 @@ export const content = {
         github: "GitHub",
       },
       toggleLanguage: "🇬🇧 EN",
-      skillsTitle: "Yeteneklerim"
+      skillsTitle: "Yeteneklerim",
+      profile: {
+        basicInfo: "Temel Bilgiler",
+        birthDate: "Doğum Tarihi",
+        city: "İkamet Şehri",
+        education: "Eğitim Durumu",
+        educationDetails: "Pamukkale Üniversitesi, İnşaat Mühendisliği, 2019",
+        preferredRole: "Tercih Ettiği Rol",
+        aboutMe: "Hakkımda",
+        aboutDescription:
+          "Ben bir front-end geliştiricisiyim ve ölçeklenebilir, kullanıcı dostu web uygulamaları yapmaya büyük bir tutkum var. Modern UI framework'leriyle çalışmayı seviyorum ve tasarımları tamamen fonksiyonel ürünlere dönüştürmeyi çok keyifli buluyorum.",
+      },
     },
   };
   
