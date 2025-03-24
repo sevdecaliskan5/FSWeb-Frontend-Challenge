f6cd38da9fc242098d2f60414b4ba41809c471a2
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 11:44:48 +0300

    skills import

---
+++ FSWeb-Frontend-Challenge/src/components/Skills.jsx
@@ -0,0 +1,7 @@
+import jsLogo from "../assets/icons/jslogo.jpg";
+import reactLogo from "../assets/icons/reactlogo.jpg";
+import reduxLogo from "../assets/icons/reduxlogo.jpg";
+import nodeLogo from "../assets/icons/nodelogo.jpg";
+import vscodeLogo from "../assets/icons/vscodelogo.jpg";
+import figmaLogo from "../assets/icons/figmalogo.jpg";
+
