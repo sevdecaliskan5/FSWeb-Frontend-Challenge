17c76df64fd95e534d4efb5ee1d8ae19908c8e77
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 14:56:20 +0300

    footer statik icerik content ile dinamiklestirildi

--- FSWeb-Frontend-Challenge/src/components/Footer.jsx
+++ FSWeb-Frontend-Challenge/src/components/Footer.jsx
@@ -1,29 +1,25 @@
+import { useTheme } from "../contexts/ThemeContext";
+import { useLanguage } from "../contexts/LanguageContext";
+import { content } from "../data/content";
+
 export default function Footer() {
+  const { darkMode } = useTheme();
+  const { language } = useLanguage();
+
+  const footerContent = content[language].footer; //content dosyasindaki footer kismini cekiyoruz
+
   return (
     <footer className="px-10 py-10 md:px-40 md:py-20 grid md:grid-cols-3 gap-10 items-center">
       <p className="col-span-2 text-3xl md:text-4xl tracking-wide leading-normal font-medium text-right md:pl-20 pr-10">
-        Let’s work together on your next product.
+        {footerContent.text}
       </p>
 
       <div className="flex justify-center md:justify-start md:flex-col gap-4 font-medium">
-        {[
-          {
-            name: "GitHub",
-            url: "https://github.com/sevdecaliskan5",
-            color: "text-sky-600",
-          },
-          { name: "Personal Blog", url: "#", color: "text-black" },
-          { name: "LinkedIn", url: "#", color: "text-cyan-800" },
-          {
-            name: "Email",
-            url: "mailto:ssevdecaliskan@gmail.com",
-            color: "text-pink-500",
-          },
-        ].map((link, index) => (
+        {footerContent.links.map((link, index) => (
           <a
             key={index}
             href={link.url}
-            className={`${link.color} hover:underline`}
+            className={`${link.color}`}
           >
             {link.name}
           </a>
