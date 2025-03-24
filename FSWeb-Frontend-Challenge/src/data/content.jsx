57301d4a19f5decbdb82508c313b486681eaee80
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 14:27:40 +0300

    langtoggle icin content olusturuldu

---
+++ FSWeb-Frontend-Challenge/src/data/content.jsx
@@ -0,0 +1,27 @@
+export const content = {
+    en: {
+      greeting: "Hi! 👋",
+      description:
+        "I'm Sevde. I’m a front-end developer. I can craft solid and scalable frontend products. Let’s meet!",
+      freelancingInfo:
+        "Currently Freelancing for UX, UI, & Web Design Projects. Invite me to join your team - ssevdecaliskan@gmail.com",
+      socialMedia: {
+        linkedin: "LinkedIn",
+        github: "GitHub",
+      },
+      toggleLanguage: "🇹🇷 TR",
+    },
+    tr: {
+      greeting: "Merhaba! 👋",
+      description:
+        "Ben Sevde. Ben bir front-end geliştiricisiyim. Sağlam ve ölçeklenebilir frontend ürünleri oluşturabilirim. Hadi tanışalım!",
+      freelancingInfo:
+        "Şu anda UX, UI ve Web Tasarımı projeleri için serbest çalışıyorum. Beni ekibinize katılmaya davet edin - ssevdecaliskan@gmail.com",
+      socialMedia: {
+        linkedin: "LinkedIn",
+        github: "GitHub",
+      },
+      toggleLanguage: "🇬🇧 EN",
+    },
+  };
+  
