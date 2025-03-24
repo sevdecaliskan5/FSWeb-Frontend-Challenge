423424b323ac9e495bac095444fc6dbcfcac4680
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 14:26:47 +0300

    theme ve lang ile app sarmalandi

--- FSWeb-Frontend-Challenge/src/App.jsx
+++ FSWeb-Frontend-Challenge/src/App.jsx
@@ -5,22 +5,26 @@ import Hero from "./components/Hero";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import { LanguageProvider } from "./contexts/LanguageContext";
 
 function App() {
   return (
-    <>
-      <header>
-        <Hero />
-      </header>
-      <main>
-        <Skills />
-        <Profile />
-        <Projects />
-      </main>
-      <footer>
-        <Footer />
-      </footer>
-    </>
+    <ThemeProvider>
+      <LanguageProvider>
+        <header>
+          <Hero />
+        </header>
+        <main>
+          <Skills />
+          <Profile />
+          <Projects />
+        </main>
+        <footer>
+          <Footer />
+        </footer>
+      </LanguageProvider>
+    </ThemeProvider>
   );
 }
 
