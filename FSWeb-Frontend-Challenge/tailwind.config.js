236f1d1d5626cafaf7293d89c5e784c71912fa4f
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 11:55:37 +0300

    import figma colors

--- FSWeb-Frontend-Challenge/tailwind.config.js
+++ FSWeb-Frontend-Challenge/tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        pink2: "#e92577",
+        yellow2: "#ffe86e",
+        bluecard: "#ddeefe",
+        greencard: "#d9f6f1",
+        blue2: "#82bbff",
+        acikkahve: "#484148",
+        koyukahve: "#2a262b",
+        koyuyesil: "#2d3235",
+        solukyesil: "#495351",
+        gri2: "#777777",
+      },
+    },
   },
   plugins: [],
-}
+};
