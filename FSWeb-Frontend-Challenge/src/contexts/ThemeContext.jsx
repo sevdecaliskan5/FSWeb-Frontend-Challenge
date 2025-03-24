4e41518500d250a26ebd76628f568c7576530e0d
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 14:26:16 +0300

    themecontext

---
+++ FSWeb-Frontend-Challenge/src/contexts/ThemeContext.jsx
@@ -0,0 +1,22 @@
+import React, { createContext, useContext, useState } from 'react';
+
+const ThemeContext = createContext();
+
+const ThemeProvider = ({ children }) => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleTheme = () => {
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ darkMode, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+const useTheme = () => useContext(ThemeContext);
+
+export { ThemeProvider, useTheme };
+
--- FSWeb-Frontend-Challenge/vite.config.js
+++ FSWeb-Frontend-Challenge/vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/FSWeb-Frontend-Challenge/", // GitHub repo adı, örneğin: '/my-project-name/'
+  base: "/FSWeb-Frontend-Challenge/", 
 });
