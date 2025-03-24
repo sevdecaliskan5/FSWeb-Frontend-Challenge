c0105eaeb302bf87262de50f35ee7fc24736d1e0
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-24 14:45:25 +0300

    profile statik icerik content ile dinamiklestirildi

--- FSWeb-Frontend-Challenge/src/components/Profile.jsx
+++ FSWeb-Frontend-Challenge/src/components/Profile.jsx
@@ -1,30 +1,37 @@
+import { useTheme } from "../contexts/ThemeContext";
+import { useLanguage } from "../contexts/LanguageContext";
+import { content } from "../data/content"; 
+
+
 export default function Profile() {
+  const { darkMode } = useTheme();
+  const { language } = useLanguage();
+
+  const profileContent = content[language].profile;
+
   return (
     <section className="px-10 py-10 md:px-40 md:py-20 flex flex-col items-center">
-      <h2 className="text-4xl font-medium tracking-wide pb-10">Profile</h2>
+      <h2 className="text-4xl font-medium tracking-wide pb-10">{profileContent.aboutMe}</h2>
 
       <div className="grid md:grid-cols-2 gap-10 w-full max-w-4xl">
         <div className="bg-white dark:bg-gray-800 rounded-2xl p-6 shadow-md">
           <h3 className="text-lg text-pink-500 font-semibold pb-4">
-            Basic Information
+          {profileContent.basicInfo}
           </h3>
           <dl className="grid grid-cols-2 gap-4 text-gray-700 dark:text-gray-300">
-            <dt className="font-bold">Doğum Tarihi</dt> <dd>05.02.1996</dd>
-            <dt className="font-bold">İkamet Şehri</dt> <dd>İzmir</dd>
-            <dt className="font-bold">Eğitim Durumu</dt>
-            <dd>Pamukkale Üniversitesi, İnşaat Mühendisliği, 2019</dd>
-            <dt className="font-bold">Tercih Ettiği Rol</dt>
+            <dt className="font-bold">{profileContent.birthDate}</dt> <dd>05.02.1996</dd>
+            <dt className="font-bold">{profileContent.city}</dt> <dd>İzmir</dd>
+            <dt className="font-bold">{profileContent.education}</dt>
+            <dd>{profileContent.educationDetails}</dd>
+            <dt className="font-bold">{profileContent.preferredRole}</dt>
             <dd>Frontend, UI</dd>
           </dl>
         </div>
 
-        <div className="flex flex-col justify-center">
-          <h3 className="text-lg font-semibold pb-4">About Me</h3>
+        <div className="text-start flex flex-col justify-center">
+          <h3 className="text-lg font-semibold pb-4">{profileContent.aboutMe}</h3>
           <p className="text-gray-700 dark:text-gray-300">
-            I am a front-end developer with a strong passion for crafting
-            scalable and user-friendly web applications. I enjoy working with
-            modern UI frameworks and love transforming designs into fully
-            functional products.
+          {profileContent.aboutDescription}
           </p>
         </div>
       </div>
