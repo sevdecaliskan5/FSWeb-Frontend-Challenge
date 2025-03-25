f90b8373ea8b56a49c6df19c97ed256606ab7358
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-25 13:46:02 +0300

    elemente erisildikten sonra useEffect kullanildi

--- src/contexts/ThemeContext.jsx
+++ src/contexts/ThemeContext.jsx
@@ -4,24 +4,24 @@ const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
   const [theme, setTheme] = useState(() => {
-    const storedTheme = localStorage.getItem('theme');
-    return storedTheme !== null ? storedTheme : 'light';
+    const storedTheme = localStorage.getItem("theme");
+    return storedTheme !== null ? storedTheme : "light";
   });
 
-
   useEffect(() => {
-    localStorage.setItem('theme', theme);
-    if(theme !== "dark"){
+    const element = document.documentElement;
+    if (theme === "dark") {
       element.classList.add("dark");
+      element.classList.remove("light");
     } else {
       element.classList.remove("dark");
+      element.classList.add("light");
     }
+    localStorage.setItem("theme", theme);
   }, [theme]);
-  
-  const element = document.documentElement;
 
   const toggleTheme = () => {
-    setTheme((curr) => curr === 'light' ? 'dark' : 'light' );
+    setTheme((curr) => (curr === "light" ? "dark" : "light"));
   };
 
   return (
