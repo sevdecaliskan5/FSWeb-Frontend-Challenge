cf9ca71697f36cba1d07a2b94ef3ea138bec84e3
sevdecaliskan5 <ssevdecaliskan@gmail.com>
2025-03-28 14:57:21 +0300

    projeler arrayine objeler eklendi

--- src/data/content.jsx
+++ src/data/content.jsx
@@ -1,3 +1,14 @@
+import amazon from "/images/amazon.png";
+import education from "/images/education.png";
+import notekeeper from "/images/note.png";
+import emaartech from "/images/emaartech.png";
+import jbl from "/images/jbl.png";
+import movie from "/images/movie.png";
+import netflix from "/images/netflix.png";
+import starbucks from "/images/starbucks.png";
+import pizza from "/images/pizza.png";
+import showcase from "/images/show.png";
+
 export const content = {
   en: {
     greeting: "Hi! 👋",
@@ -27,52 +38,111 @@ export const content = {
       aboutDescription:
         "I was born in 1996 in Izmir and have a degree in Civil Engineering. My passion for design and technology led me to Front-End development, where I focus on enhancing my skills. I continuously improve myself through both independent learning and various courses in software development. I build user-friendly designs using Front-End languages and technologies like React and TailwindCSS. Learning new technologies and applying them in my projects is something I enjoy.",
     },
-
     projects: {
       title: "Projects",
       list: [
         {
           projectTitle: "PizzaPal",
           projectDescription:
-            "A React-based pizza order application where users can select pizzas, add toppings, and place an order. The app calculates the total price based on user selections.",
+            "A React-based pizza ordering application where users can select pizzas, add ingredients, and automatically calculate the total price.",
           tags: ["JavaScript", "React", "CSS"],
-          githubLink: "#",
-          appLink: "#",
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://fsweb-s8-challenge-pizza-omega.vercel.app/",
+          img: pizza,
         },
         {
-          projectTitle: "Movie Finder",
+          projectTitle: "Showcase",
           projectDescription:
-            "An app that allows users to search for movies using the OMDB API and view movie details.",
-          tags: ["JavaScript", "React", "API"],
-          githubLink: "#",
-          appLink: "#",
-        },
-        {
-          projectTitle: "QuizMaster",
-          projectDescription:
-            "A quiz application that allows users to answer multiple-choice questions and get their results at the end.",
-          tags: ["JavaScript", "React", "CSS"],
-          githubLink: "#",
-          appLink: "#",
+            "A modern and responsive portfolio site featuring an interactive terminal-style section and dark/light mode toggle.",
+          tags: ["JavaScript", "React", "Tailwind"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://my-portfolio-n5mk.vercel.app/",
+          img: showcase,
         },
         {
           projectTitle: "CalcMaster",
           projectDescription:
-            "A simple calculator app where users can perform basic mathematical operations like addition, subtraction, multiplication, and division. It provides an intuitive UI and real-time calculations.",
+            "A simple calculator app that allows users to perform basic mathematical operations such as addition, subtraction, multiplication, and division.",
           tags: ["JavaScript", "React", "CSS"],
-          githubLink: "#",
+          githubLink: "https://github.com/sevdecaliskan5",
           appLink: "#",
+          img: "",
         },
         {
-          projectTitle: "ToDoMaster",
+          projectTitle: "Note Keeper",
           projectDescription:
-            "A simple to-do list application where users can add, edit, and delete tasks. It uses LocalStorage to persist data even after the browser is closed.",
+            "A simple to-do list application where users can add, edit, and delete tasks. Data is stored using LocalStorage to persist even after closing the browser.",
           tags: ["JavaScript", "CSS"],
-          githubLink: "#",
-          appLink: "#",
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://note-keeper-silk-seven.vercel.app/",
+          img: notekeeper,
+        },
+        {
+          projectTitle: "Movie Finder",
+          projectDescription:
+            "A movie discovery site listing popular, top-rated, and newly released movies, including posters, descriptions, and genre details.",
+          tags: ["JavaScript", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://movie-web-site-ten.vercel.app/",
+          img: movie,
+        },
+        {
+          projectTitle: "Netflix Clone",
+          projectDescription:
+            "A replica of Netflix's homepage built with HTML and CSS. The responsive design ensures compatibility with different screen sizes.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://netflix-clone-rho-coral-22.vercel.app/",
+          img: netflix,
+        },
+        {
+          projectTitle: "EmaarTech",
+          projectDescription:
+            "A user-friendly real estate listing site featuring properties for sale and rent, complete with photos, descriptions, prices, and location details.",
+          tags: ["HTML", "Bootstrap"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://real-estate-chi-sepia.vercel.app/",
+          img: emaartech,
+        },
+        {
+          projectTitle: "JBL",
+          projectDescription:
+            "A website built with HTML and SCSS showcasing different JBL headphone models, their features, and best-selling products with a responsive design.",
+          tags: ["HTML", "SCSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://jbl-clone-eight.vercel.app/",
+          img: jbl,
+        },
+        {
+          projectTitle: "Udemig Education",
+          projectDescription:
+            "A clean and user-friendly educational platform featuring software courses with detailed information on course structures.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://education-web-site-one.vercel.app/",
+          img: education,
+        },
+        {
+          projectTitle: "Amazon Clone",
+          projectDescription:
+            "A project replicating Amazon's homepage design, including product categories and promotional banners for an authentic Amazon experience.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://amazon-clone-zeta-peach.vercel.app/",
+          img: amazon,
+        },
+        {
+          projectTitle: "Starbucks Clone",
+          projectDescription:
+            "A Starbucks-themed website that stays true to its color palette, typography, and design, offering an intuitive interface for exploring products and services.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://starbucks-clone-six-pi.vercel.app/",
+          img: starbucks,
         },
       ],
     },
+
     footer: {
       text: "Let’s work together on your next product.",
       links: [
@@ -129,46 +199,104 @@ export const content = {
         {
           projectTitle: "PizzaPal",
           projectDescription:
-            "Kullanıcıların pizza seçip, malzeme ekleyerek sipariş verebildiği bir React tabanlı pizza sipariş uygulaması. Uygulama, kullanıcı seçimlerine göre toplam fiyatı hesaplar.",
+            "Kullanıcıların pizza seçip malzeme ekleyerek sipariş verebildiği, toplam tutarın otomatik hesaplandığı React tabanlı bir pizza sipariş uygulaması.",
           tags: ["JavaScript", "React", "CSS"],
-          githubLink: "#",
-          appLink: "#",
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://fsweb-s8-challenge-pizza-omega.vercel.app/",
+          img: pizza,
         },
         {
-          projectTitle: "Movie Finder",
+          projectTitle: "Showcase",
           projectDescription:
-            "Kullanıcıların OMDB API kullanarak film arayabileceği ve film detaylarını görüntüleyebileceği bir uygulama.",
-          tags: ["JavaScript", "React", "API"],
-          githubLink: "#",
-          appLink: "#",
-        },
-        {
-          projectTitle: "QuizMaster",
-          projectDescription:
-            "Kullanıcıların çoktan seçmeli soruları yanıtlayıp sonunda sonuçlarını görebileceği bir quiz uygulaması.",
-          tags: ["JavaScript", "React", "CSS"],
-          githubLink: "#",
-          appLink: "#",
+            "Modern, duyarlı bir tasarıma sahip olup terminal tarzında interaktif bir bölüm, karanlık/açık mod geçişi sunan bir portföy sitesi.",
+          tags: ["JavaScript", "React", "Tailwind"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://my-portfolio-n5mk.vercel.app/",
+          img: showcase,
         },
         {
           projectTitle: "CalcMaster",
           projectDescription:
-            "Kullanıcıların toplama, çıkarma, çarpma ve bölme gibi temel matematiksel işlemleri yapabildiği basit bir hesap makinesi uygulaması. Kullanıcı dostu bir arayüze ve gerçek zamanlı hesaplamalara sahiptir.",
+            "Kullanıcıların toplama, çıkarma, çarpma ve bölme gibi temel matematiksel işlemleri yapabildiği basit bir hesap makinesi uygulaması.",
           tags: ["JavaScript", "React", "CSS"],
-          githubLink: "#",
+          githubLink: "https://github.com/sevdecaliskan5",
           appLink: "#",
+          img: "",
         },
         {
-          projectTitle: "ToDoMaster",
+          projectTitle: "Note Keeper",
           projectDescription:
-            "Kullanıcıların görev ekleyip, düzenleyip, silebildiği basit bir yapılacaklar listesi uygulaması. Veriler, tarayıcı kapatılsa bile kaydedilmesi için LocalStorage kullanılarak saklanır.",
+            "Kullanıcıların görev ekleyip düzenleyebildiği ve silebildiği basit bir yapılacaklar listesi uygulaması. Veriler, tarayıcı kapatıldığında bile saklanması için LocalStorage kullanılarak kaydedilir.",
           tags: ["JavaScript", "CSS"],
-          githubLink: "#",
-          appLink: "#",
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://note-keeper-silk-seven.vercel.app/",
+          img: notekeeper,
+        },
+        {
+          projectTitle: "Movie Finder",
+          projectDescription:
+            "Popüler, en yüksek puanlı ve yeni çıkan filmleri listeleyen bir film keşif sitesi. Filmlerle ilgili afiş, açıklama ve tür bilgileri içermektedir.",
+          tags: ["JavaScript", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://movie-web-site-ten.vercel.app/",
+          img: movie,
+        },
+        {
+          projectTitle: "Netflix Clone",
+          projectDescription:
+            "HTML ve CSS ile geliştirilmiş, Netflix'in ana sayfa tasarımını taklit eden bir arayüz. Duyarlı tasarımı sayesinde farklı ekran boyutlarına uyum sağlar.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://netflix-clone-rho-coral-22.vercel.app/",
+          img: netflix,
+        },
+        {
+          projectTitle: "EmaarTech",
+          projectDescription:
+            "Satılık ve kiralık emlak ilanlarını listeleyen kullanıcı dostu bir arayüz. İlanların fotoğrafları, açıklamaları, fiyatları ve konum bilgileri bulunmaktadır.",
+          tags: ["HTML", "Bootstrap"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://real-estate-chi-sepia.vercel.app/",
+          img: emaartech,
+        },
+        {
+          projectTitle: "JBL",
+          projectDescription:
+            "HTML ve SCSS kullanılarak oluşturulan bir site. Farklı JBL kulaklık modellerini, özelliklerini ve en çok satanları tanıtan duyarlı bir tasarım sunar.",
+          tags: ["HTML", "SCSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://jbl-clone-eight.vercel.app/",
+          img: jbl,
+        },
+        {
+          projectTitle: "Udemig Education",
+          projectDescription:
+            "Yazılım kurslarını içeren temiz ve kullanıcı dostu bir eğitim platformu. Kurs yapıları hakkında detaylı bilgiler sunar.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://education-web-site-one.vercel.app/",
+          img: education,
+        },
+        {
+          projectTitle: "Amazon Clone",
+          projectDescription:
+            "Amazon ana sayfasının tasarımını taklit eden bir proje. Ürün kategorileri ve promosyon afişleriyle Amazon deneyimini sunar.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://amazon-clone-zeta-peach.vercel.app/",
+          img: amazon,
+        },
+        {
+          projectTitle: "Starbucks Clone",
+          projectDescription:
+            "Starbucks'ın renk paleti, tipografisi ve tasarımına sadık kalan bir web sitesi. Ürünleri ve hizmetleri keşfetmek için kullanıcı dostu bir arayüz sunar.",
+          tags: ["HTML", "CSS"],
+          githubLink: "https://github.com/sevdecaliskan5",
+          appLink: "https://starbucks-clone-six-pi.vercel.app/",
+          img: starbucks,
         },
       ],
     },
-    
 
     footer: {
       text: "Hadi birlikte bir sonraki ürününüzü geliştirelim.",
